candy2001/android_device_xiaomi_sdm660-common

sdm660-common: Add dummy perf profile.
Since lineageos 15.1 the power hal doesn't use the
perf_lock_use_profile() method anymore.
It was used to apply performance profiles.
On startup the perfd daemon is looking for at least
one profile which is used as default.
So there is only a perf-profiles0.conf to make the
daemon happy.
Removes log spam:
E ANDR-PERF-MPCTL: Invalid profile no. 0, total profiles 0 only
Signed-off-by: Atman <masteratman@gmail.com>

sdm660.mk
@@ -344,6 +344,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# QTI Performance
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \

Ruega/ct

Change-Id: I0bf7ddb76864d597c3e6d334eddce51f20710381

sdm660.mk
@@ -173,6 +173,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# QTI Performance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \